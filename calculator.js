3b439d67482a093ff53069899de8d95abe5f70b5
AnLaxina <anilovlaxina@gmail.com>
2025-07-29 10:42:25 -0700

    Add working test cases for the rest of the Calculator object

--- calculator.js
+++ calculator.js
@@ -2,4 +2,16 @@ export class Calculator {
   static add(num1, num2) {
     return num1 + num2;
   }
+
+  static subtract(num1, num2) {
+    return num1 - num2;
+  }
+
+  static multiply(num1, num2) {
+    return num1 * num2;
+  }
+
+  static divide(num1, num2) {
+    return num1 / num2;
+  }
 }
--- calculator.test.js
+++ calculator.test.js
@@ -3,12 +3,12 @@ import { Calculator } from "./calculator";
 test("Adding 2 + 2 from Calculator should be 4", () => {
   expect(Calculator.add(2, 2)).toEqual(4);
 });
-test("Adding 2 + 2 from Calculator should be 4", () => {
-  expect(Calculator.add(2, 2)).toEqual(4);
+test("Subtracting 2 - 2 from Calculator should be 0", () => {
+  expect(Calculator.subtract(2, 2)).toEqual(0);
 });
-test("Adding 2 + 2 from Calculator should be 4", () => {
-  expect(Calculator.add(2, 2)).toEqual(4);
+test("Multiplying 2 * 3 from Calculator should be 6", () => {
+  expect(Calculator.multiply(2, 3)).toEqual(6);
 });
-test("Adding 2 + 2 from Calculator should be 4", () => {
-  expect(Calculator.add(2, 2)).toEqual(4);
+test("Dividing 8 / 8 from Calculator should be 1", () => {
+  expect(Calculator.divide(8, 8)).toEqual(1);
 });
