7c4bdc9a65f227a4230420ff468696d777718c8c
AnLaxina <anilovlaxina@gmail.com>
2025-07-29 10:39:54 -0700

    Add working test case for the Calculator's Add method

---
+++ calculator.test.js
@@ -0,0 +1,14 @@
+import { Calculator } from "./calculator";
+
+test("Adding 2 + 2 from Calculator should be 4", () => {
+  expect(Calculator.add(2, 2)).toEqual(4);
+});
+test("Adding 2 + 2 from Calculator should be 4", () => {
+  expect(Calculator.add(2, 2)).toEqual(4);
+});
+test("Adding 2 + 2 from Calculator should be 4", () => {
+  expect(Calculator.add(2, 2)).toEqual(4);
+});
+test("Adding 2 + 2 from Calculator should be 4", () => {
+  expect(Calculator.add(2, 2)).toEqual(4);
+});
